0e5af44ca54b52bb51d913b65de4a7bdf7b36e40
Geert Barentsen <geert@barentsen.be>
2012-08-30 19:27:04 +0100

    Plot of ellipticity evolution.

---
+++ qcdata/plots/ellipticity-evolution.py
@@ -0,0 +1,57 @@
+from pylab import *
+import numpy as np
+import pyfits
+
+d = pyfits.getdata("../mikes-dqc-data.fits", 1)
+
+#for t in d.time:
+#	print t
+#	datetime.datetime.strptime(t[0:16], "%Y-%m-%d %H:%M")
+
+t = np.array([datetime.datetime.strptime(t[0:10], "%Y-%m-%d") for t in d.time])
+
+years = range(2003,2011) + [2012]
+fractions, n_bad, n_all = [], [], []
+
+for myyear in years:
+	begin = datetime.datetime(myyear, 1, 1)
+	end = datetime.datetime(myyear+1, 1, 1)
+	mask = (t > begin) & (t < end)
+
+	el = d.ellipt[mask]
+	my_n_bad = len(el[el > 0.2])
+	my_n_all = len(el)
+	myfraction = my_n_bad / float(my_n_all)
+
+	n_bad.append( my_n_bad )
+	n_all.append( my_n_all )
+	fractions.append( myfraction )
+
+
+for varname in ["years", "n_bad", "n_all", "fractions"]:
+	vars()[varname] = np.array(vars()[varname])
+
+
+figure()
+title("How many IPHAS exposures had ellipticity > 0.2?", fontsize=26)
+xlabel("Year", fontsize=22)
+ylabel("Fraction [%]", fontsize=22)
+#scatter(t, d.ellipt)
+scatter(years, fractions*100, s=60)
+
+for i, myyear in enumerate(years):
+	offset = 0.45
+	if myyear == 2007:
+		offset = -0.2
+	text(myyear, fractions[i]*100-offset, "%s/%s" % (n_bad[i], n_all[i]), \
+		horizontalalignment="center")
+
+ax = axes()
+loc = matplotlib.ticker.FixedLocator(years)
+ax.xaxis.set_major_locator(loc)
+
+
+xlim([2002,2013])
+#ylim([0,8])
+savefig("iphas-ellipticity-evolution.png", dpi=120)
+show()
